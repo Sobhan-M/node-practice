dc9bc27d7cde39d18d7a172efee8718a608fdf26
Sobhan <72103946+Sobhan-M@users.noreply.github.com>
2022-09-13 14:19:37 -0400

    Connected To Database And Set Up Model

--- p2/app.js
+++ p2/app.js
@@ -1,9 +1,26 @@
+// Needed packages.
 const bodyParser = require('body-parser');
-
 const express = require('express');
-const app = express();
 
-app.use(bodyParser.urlencoded({extended: false}));
+// Models.
+const Character = require('./models/character');
+const Player = require('./models/player');
+
+// Custom files.
+const sequelize = require('./util/database');
+
+// Setting up server.
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static("public"));
 
-app.listen(8000)
+// Database management.
+Character.belongsTo(Player, {onDelete: 'CASCADE'});
+Player.hasMany(Character);
+
+// Starting server.
+sequelize.sync()
+	.then(result => {
+		app.listen(8000);
+	})
+	.catch(err => console.log(err));
